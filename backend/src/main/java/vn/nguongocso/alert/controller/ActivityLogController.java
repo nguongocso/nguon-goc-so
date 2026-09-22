cea76275afbc873d17a57eba4f6832895b6efe17
tpd05 <doantran28092005@gmail.com>
2026-09-22 10:11:17 +0700

    fix major

--- backend/src/main/java/vn/nguongocso/alert/controller/ActivityLogController.java
+++ backend/src/main/java/vn/nguongocso/alert/controller/ActivityLogController.java
@@ -22,7 +22,13 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import vn.nguongocso.auth.service.CustomUserDetails;
 import vn.nguongocso.common.ApiResult;
